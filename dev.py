d26f641bb2c059fe73b8a214698612fbee62033e
wind2sing <windsing.me@gmail.com>
2021-07-13 20:11:42 +0800

    dev debug logging

--- dev.py
+++ dev.py
@@ -12,8 +12,16 @@ port = 6002
 # 绑定域名, 含协议字段不含端口号
 domain = "http://localhost"
 
+from api.router import APIRouter
+import logging
+logger = logging.getLogger('anime')
+
 
 if __name__ == '__main__':
     app = APIRouter(host, port)
     app.set_domain(domain)
+    for handler in logger.handlers:
+        if isinstance(handler, type(logging.StreamHandler())):
+            handler.setLevel(logging.DEBUG)
+            logger.debug('Debug logging enabled')
     app.run()
